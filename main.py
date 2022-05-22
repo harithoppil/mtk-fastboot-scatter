973ec5a093586784235f5239574a71e97bab92ee
Gagan Malvi <malvigagan@gmail.com>
2022-05-22 16:41:26 +0530

    feat(flashing): Functionalize and add argument support.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- main.py
+++ main.py
@@ -16,25 +16,28 @@ import tools.parse as parser
 import tools.fastboot as fastboot
 import tools.adb as adb
 import tools.warnings as warnings
+import sys
 from tabulate import tabulate
 
-# TODO: Take path from cmdline
-path = "/home/malvi/Downloads/Teracube_2e_06_2020/Teracube_2e_06_20201116/MT6765_Android_scatter.txt"
+def flash(path):
+    print("[-] Generating partition table...")
+    partitions = parser.partition_list_generate(path)
 
-partitions = parser.partition_list_generate(path)
+    print(warnings.aboutwarning)
 
-print(warnings.aboutwarning)
+    partitionsList = []
+    for case in partitions:
+        partitionsList.append([case, partitions[case]])
 
-# TODO: Make it functional
-partitionsList = []
+    print(tabulate(partitionsList, headers = ['Partition', 'Image'], tablefmt = "fancy_grid"))
 
-for case in partitions:
-    partitionsList.append([case, partitions[case]])
+    print("[-] Flashing images...")
+    for case in partitions:
+        fastboot.flashPartition(case, partitions[case])
+        print("[*] Flashed " + case + " successfully.")
 
-print(tabulate(partitionsList, headers=["Partition", "Filename"], tablefmt="fancy_grid"))
-
-# Begin flash
-print("Beginning flash...")
-for case in partitions:
-    fastboot.flashPartition(case, partitions[case])
-    print("Flashed " + case)
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        flash(sys.argv[1])
+    else:
+        print("[-] Please provide a path to the scatter file in the SP Flash package.")
